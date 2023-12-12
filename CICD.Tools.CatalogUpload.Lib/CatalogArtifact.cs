cdf7af7f31443a720563d47909c6000a05af026b
Jan Staelens <jan.staelens@skyline.be>
2023-12-12 11:00:03 +0100

    Update CICD.Tools.CatalogUpload.Lib/CatalogArtifact.cs
    Co-authored-by: Pedro Debevere <97611683+PedroDebevere@users.noreply.github.com>

--- CICD.Tools.CatalogUpload.Lib/CatalogArtifact.cs
+++ CICD.Tools.CatalogUpload.Lib/CatalogArtifact.cs
@@ -15,7 +15,7 @@
 	/// Allows Uploading an artifact to the Catalog using one of the below in order of priority:
 	///  <para>- provided key in upload argument (Unix/Windows)</para>
 	///  <para>- key stored as an Environment Variable called "dmcatalogtoken". (Unix/Windows)</para>
-	///  <para>- key configured using Skyline.DataMiner.CICD.Tools.WinEncryptedKeys called "dmcatalogtoken_encrypted" (windows only)</para>
+	///  <para>- key configured using Skyline.DataMiner.CICD.Tools.WinEncryptedKeys called "dmcatalogtoken_encrypted" (Windows only)</para>
 	/// </summary>
 	public class CatalogArtifact
 	{
